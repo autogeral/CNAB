ce676e0c8340c56a72e2f6c58ba9dae952b59106
cesariolange <cesariolange@5adba155-f38a-43c0-aea0-d88c706dde45>
2013-05-31 13:56:30 +0000

    [update] ajustes nos campos
    git-svn-id: https://svn.autogeral.com.br/code/CNAB@2967 5adba155-f38a-43c0-aea0-d88c706dde45

--- test/br/com/jcomputacao/cnab/t240v05/extrato/LineCnabSegmentoEExtratoTest.java
+++ test/br/com/jcomputacao/cnab/t240v05/extrato/LineCnabSegmentoEExtratoTest.java
@@ -4,6 +4,7 @@ import br.com.jcomputacao.aristoteles.line.LineArchetype;
 import br.com.jcomputacao.aristoteles.line.LineModel;
 import br.com.jcomputacao.cnab.t240.v05.extrato.LineCnabv05SegmentoEExtrato;
 import java.text.ParseException;
+import java.util.Date;
 import org.junit.*;
 
 /**
@@ -12,6 +13,7 @@ import org.junit.*;
  */
 public class LineCnabSegmentoEExtratoTest {
     //Método construtor
+
     public LineCnabSegmentoEExtratoTest() {
     }
 
@@ -35,8 +37,8 @@ public class LineCnabSegmentoEExtratoTest {
     public void LineCnabSegmentoEExtratoTest() throws ParseException {
         LineArchetype la = new LineCnabv05SegmentoEExtrato();
         LineModel line = la.createModel();
-        line.setRepresentation("3410001300001E1  205437537000137000000         34202002782000000092471 7AUTO GERAL DE ITU LTDA.             DPV                      S        17052013000000000000539221C2060048RES APLIC AUT MAIS                                        000000");
-        
+        line.setRepresentation("3410001300001E1  212924343000121000000         34201002782000000090749 8WALZLAGER FABRIK DO BR LTDA         DPV                      S        20052013000000000000000601C2050000DESBLOQUEIO JUDICIAL                                      000000");
+
         //01
         int codigoBanco = line.getFieldValueNumber(LineCnabv05SegmentoEExtrato.CODIGO_BANCO).intValue();
         //02
@@ -54,7 +56,7 @@ public class LineCnabSegmentoEExtratoTest {
         //08
         String tipoInscricaoCliente = line.getFieldValueString(LineCnabv05SegmentoEExtrato.TIPO_INSCRICAO);
         //09
-        int numeroInscricao = line.getFieldValueNumber(LineCnabv05SegmentoEExtrato.NUMERO_INSCRICAO).intValue();
+        long numeroInscricao = line.getFieldValueNumber(LineCnabv05SegmentoEExtrato.NUMERO_INSCRICAO).longValue();
         //10
         String brancos2 = line.getFieldValueString(LineCnabv05SegmentoEExtrato.BRANCOS2);
         //11
@@ -80,25 +82,110 @@ public class LineCnabSegmentoEExtratoTest {
         //21
         String naturezaLancamento = line.getFieldValueString(LineCnabv05SegmentoEExtrato.NATUREZA_LANCAMENTO);
         //22
-        int tipoComplementoLancamento = line.getFieldValueNumber(LineCnabv05SegmentoEExtrato.TIPO_COMPLEMENTO).intValue();
+        int tipoComplementoLancamento = 0;
+        if (line.getFieldValueNumber(LineCnabv05SegmentoEExtrato.TIPO_COMPLEMENTO) != null) {
+            tipoComplementoLancamento = line.getFieldValueNumber(LineCnabv05SegmentoEExtrato.TIPO_COMPLEMENTO).intValue();
+        }
         //23
+        int bancoOrigemLancamento = 0;
+        if (line.getFieldValueNumber(LineCnabv05SegmentoEExtrato.BANCO_ORIGEM_LANCAMENTO) != null) {
+            bancoOrigemLancamento = line.getFieldValueNumber(LineCnabv05SegmentoEExtrato.BANCO_ORIGEM_LANCAMENTO).intValue();
+        }
         //24
+        int agenciaOrigemLancamento = 0;
+        if (line.getFieldValueNumber(LineCnabv05SegmentoEExtrato.AGENCIA_ORIGEM_LANCAMENTO) != null) {
+            agenciaOrigemLancamento = line.getFieldValueNumber(LineCnabv05SegmentoEExtrato.AGENCIA_ORIGEM_LANCAMENTO).intValue();
+        }
         //25
+        long agenciaContaOrigemLancamento = 0;
+        if (line.getFieldValueNumber(LineCnabv05SegmentoEExtrato.AG_CONTA_ORIGEM_LANCAMENTO) != null) {
+            agenciaContaOrigemLancamento = line.getFieldValueNumber(LineCnabv05SegmentoEExtrato.AG_CONTA_ORIGEM_LANCAMENTO).longValue();
+        }
         //26
+        String idenfiticacaoIsencaoCpmf = line.getFieldValueString(LineCnabv05SegmentoEExtrato.IDENTIFICACAO_INSENCAO_CPMF);
         //27
+        Date dataContabil = line.getFieldValueDate(LineCnabv05SegmentoEExtrato.DATA_CONTABIL);
         //28
+        Date dataLancamento = line.getFieldValueDate(LineCnabv05SegmentoEExtrato.DATA_LANCAMENTO);
         //29
+        double valorLancamento = line.getFieldValueNumber(LineCnabv05SegmentoEExtrato.VALOR_LANCAMENTO).doubleValue();
         //30
+        String tipoLancamento = line.getFieldValueString(LineCnabv05SegmentoEExtrato.TIPO_LANCAMENTO);
         //31
+        int categoriaLancamento = line.getFieldValueNumber(LineCnabv05SegmentoEExtrato.CATEGORIA_LANCAMENTO).intValue();
         //32
+        String codigoLancamento = line.getFieldValueString(LineCnabv05SegmentoEExtrato.CODIGO_LANCAMENTO);
         //33
+        String historico = line.getFieldValueString(LineCnabv05SegmentoEExtrato.HISTORICO_LANCAMENTO);
         //34
+        int agenciaDeOrigemLancamento = 0;
+        if (line.getFieldValueNumber(LineCnabv05SegmentoEExtrato.AG_ORIGEM_LANCAMENTO) != null) {
+            agenciaDeOrigemLancamento = line.getFieldValueNumber(LineCnabv05SegmentoEExtrato.AG_ORIGEM_LANCAMENTO).intValue();
+        }
         //35
+        int complemento = 0;
+        if (line.getFieldValueNumber(LineCnabv05SegmentoEExtrato.COMPLEMENTO_REGISTRO) != null) {
+            complemento = line.getFieldValueNumber(LineCnabv05SegmentoEExtrato.COMPLEMENTO_REGISTRO).intValue();
+        }
         //36
+        int contaDeOrigemLancamento = 0;
+        if (line.getFieldValueNumber(LineCnabv05SegmentoEExtrato.CONTA_ORIGEM_LANCAMENTO) != null) {
+            contaDeOrigemLancamento = line.getFieldValueNumber(LineCnabv05SegmentoEExtrato.CONTA_ORIGEM_LANCAMENTO).intValue();
+        }
         //37
+        int dacAgContaOrigem = 0;
+        if (line.getFieldValueNumber(LineCnabv05SegmentoEExtrato.DAC_AG_CONTA_ORIGEM) != null){
+            dacAgContaOrigem = line.getFieldValueNumber(LineCnabv05SegmentoEExtrato.DAC_AG_CONTA_ORIGEM).intValue();
+        }
         //38
+        String tipoInscricaoEmitente = line.getFieldValueString(LineCnabv05SegmentoEExtrato.TIPO_INSCRICAO_EMITENTE);
         //39
+        String numeroInscricaoEmitente = line.getFieldValueString(LineCnabv05SegmentoEExtrato.NUMERO_INSCRICAO_EMITENTE);   
         //40
+        String brancos4 = line.getFieldValueString(LineCnabv05SegmentoEExtrato.BRANCOS4);
         //41
+        String numeroDocumento = line.getFieldValueString(LineCnabv05SegmentoEExtrato.NUMERO_DOCUMENTO);
+        
+        System.out.println("01. Código do banco: " + codigoBanco);
+        System.out.println("02. Código do lote: " + codigoLote);
+        System.out.println("03. Tipo de registro: " + tipoRegistro);
+        System.out.println("04. Número do registro: " + numeroRegistro);
+        System.out.println("05. Segmento: " + codigoSegmento);
+        System.out.println("06. Lançamento: " + identificacaoTipoLancamento);
+        System.out.println("07. Brancos1: " + brancos);
+        System.out.println("08. Tipo de inscrição do cliente: " + tipoInscricaoCliente);
+        System.out.println("09. Número de inscrição do cliente: " + numeroInscricao);
+        System.out.println("10. Brancos2: " + brancos2);
+        System.out.println("11. Convênio: " + convenio);
+        System.out.println("12. Zeros1: " + zeros);
+        System.out.println("13. Agência mantenedora: " + agenciaMantenedora);
+        System.out.println("14. Dígito verificados da agência: " + digitoVerificadorAgencia);
+        System.out.println("15. Zeros2: " + zeros2);
+        System.out.println("16. Número da conta corrente: " + numeroContaCorrente);
+        System.out.println("17. Brancos3: " + brancos3);
+        System.out.println("18. Dígito verificados da agência/conta: " + digitoVerificadosAgConta);
+        System.out.println("19. Nome da empresa: " + nomeEmpresa);
+        System.out.println("20. Reservado uso banco: " + reservadoUsoBranco);
+        System.out.println("21. Natureza do lançamento: " + naturezaLancamento);
+        System.out.println("22. Tipo do complemento: " + tipoComplementoLancamento);
+        System.out.println("23. Banco origem do lançamento: " + bancoOrigemLancamento);
+        System.out.println("24. Agência origem do lançamento: " + agenciaOrigemLancamento);
+        System.out.println("25. Ag./Conta origem do lançamento: " + agenciaContaOrigemLancamento);
+        System.out.println("26. Identificaçãoi de isenção de CPMF: " + idenfiticacaoIsencaoCpmf);
+        System.out.println("27. Data contábil: " + dataContabil);
+        System.out.println("28. Data lançamento: " + dataLancamento);
+        System.out.println("29. Valor do lançamento: " + valorLancamento);
+        System.out.println("30. Tipo do lançamento: " + tipoLancamento);
+        System.out.println("31. Categoria do lançamento: " + categoriaLancamento);
+        System.out.println("32. Código do lançamento: " + codigoLancamento);
+        System.out.println("33. Histórico: " + historico);
+        System.out.println("34. Agência de origem do lançamento: " + agenciaDeOrigemLancamento);
+        System.out.println("35. Complemento de registro: " + complemento);
+        System.out.println("36. Conta de origem do laçamento: " + contaDeOrigemLancamento);
+        System.out.println("37. DAC: " + dacAgContaOrigem);
+        System.out.println("38. Tipo de inscrição do emitente: " + tipoInscricaoEmitente);
+        System.out.println("39. Número de inscrição do emitente: " + numeroInscricaoEmitente);
+        System.out.println("40. Brancos4: " + brancos4);
+        System.out.println("41. Número do documento: " + numeroDocumento);
     }
 }
