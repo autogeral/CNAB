4cc9da338040be481f449c6d2c60bb2fe6a038f9
cesariolange <cesariolange@5adba155-f38a-43c0-aea0-d88c706dde45>
2013-07-05 14:21:57 +0000

    [ADD] SEGMENTO E DE ACORDO COM O BANCO ITAU SEGUE OS PADROES DA FEBRABAN.
    git-svn-id: https://svn.autogeral.com.br/code/CNAB@3235 5adba155-f38a-43c0-aea0-d88c706dde45

--- src/br/com/jcomputacao/cnab/t240/v05/extrato/LineCnabv05SegmentoEExtratoItau.java
+++ src/br/com/jcomputacao/cnab/t240/v05/extrato/LineCnabv05SegmentoEExtratoItau.java
@@ -6,9 +6,9 @@ import br.com.jcomputacao.cnab.t240.LineCnab240SegmentoE;
  *
  * @author Jennifer
  */
-public class LineCnabv05SegmentoEExtrato extends LineCnab240SegmentoE {
+public class LineCnabv05SegmentoEExtratoItau extends LineCnab240SegmentoE {
 
-    public LineCnabv05SegmentoEExtrato() {
+    public LineCnabv05SegmentoEExtratoItau() {
         setName("CNAB 240 v5.0 - Segmento E - Extrato Conta Corrente");
     }
 }
