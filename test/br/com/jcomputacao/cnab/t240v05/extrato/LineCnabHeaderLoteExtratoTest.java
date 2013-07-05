c99cd5f7b24c215c0d628deebbd832acf9c39a7c
cesariolange <cesariolange@5adba155-f38a-43c0-aea0-d88c706dde45>
2013-07-05 14:17:22 +0000

    [UPDATE] HOUVE UMA MUDANCA NO TRATAMENTO DO LAYOUT DO ARQUIVO DE CONCILIACAO EM FUNCAO DE CAMPOS COM MUDANCAS DRASTICAS EM LAYOUTS DISTINTOS.
    git-svn-id: https://svn.autogeral.com.br/code/CNAB@3228 5adba155-f38a-43c0-aea0-d88c706dde45

--- test/br/com/jcomputacao/cnab/t240v05/extrato/LineCnabHeaderLoteExtratoTest.java
+++ test/br/com/jcomputacao/cnab/t240v05/extrato/LineCnabHeaderLoteExtratoTest.java
@@ -2,7 +2,7 @@ package br.com.jcomputacao.cnab.t240v05.extrato;
 
 import br.com.jcomputacao.aristoteles.line.LineArchetype;
 import br.com.jcomputacao.aristoteles.line.LineModel;
-import br.com.jcomputacao.cnab.t240.v05.extrato.LineCnab240v05HeaderLoteExtrato;
+import br.com.jcomputacao.cnab.t240.v05.extrato.LineCnab240v05HeaderLoteExtratoLayout031;
 import java.text.ParseException;
 import java.util.Date;
 import org.junit.*;
@@ -36,68 +36,68 @@ public class LineCnabHeaderLoteExtratoTest {
     @Test
     public void LineCnabHeaderLoteExtratoTest() throws ParseException {
 
-        LineArchetype la = new LineCnab240v05HeaderLoteExtrato();
+        LineArchetype la = new LineCnab240v05HeaderLoteExtratoLayout031();
         LineModel line = la.createModel();
         line.setRepresentation("34100011E0440031 212924343000121               34201002782000000090749 8WALZLAGER FABRIK DO BR LTDA                                           16052013000000000000005000CPBRL00001                                                              ");
 
         //01
-        String codigoBanco = line.getFieldValueString(LineCnab240v05HeaderLoteExtrato.CODIGO_BANCO);
+        String codigoBanco = line.getFieldValueString(LineCnab240v05HeaderLoteExtratoLayout031.CODIGO_BANCO);
         //02
-        int loteServico = line.getFieldValueNumber(LineCnab240v05HeaderLoteExtrato.LOTE_SERVICO).intValue();
+        int loteServico = line.getFieldValueNumber(LineCnab240v05HeaderLoteExtratoLayout031.LOTE_SERVICO).intValue();
         //03
-        String tipoRegistro = line.getFieldValueString(LineCnab240v05HeaderLoteExtrato.TIPO_REGISTRO);
+        String tipoRegistro = line.getFieldValueString(LineCnab240v05HeaderLoteExtratoLayout031.TIPO_REGISTRO);
         //04
-        String tipoOperacao = line.getFieldValueString(LineCnab240v05HeaderLoteExtrato.TIPO_OPERACAO);
+        String tipoOperacao = line.getFieldValueString(LineCnab240v05HeaderLoteExtratoLayout031.TIPO_OPERACAO);
         //05
-        int tipoServico = line.getFieldValueNumber(LineCnab240v05HeaderLoteExtrato.TIPO_SERVICO).intValue();
+        int tipoServico = line.getFieldValueNumber(LineCnab240v05HeaderLoteExtratoLayout031.TIPO_SERVICO).intValue();
         //06
-        int formaLancamento = line.getFieldValueNumber(LineCnab240v05HeaderLoteExtrato.FORMA_LANCAMENTO).intValue();
+        int formaLancamento = line.getFieldValueNumber(LineCnab240v05HeaderLoteExtratoLayout031.FORMA_LANCAMENTO).intValue();
         //07
-        String numeroVersaoLayout = line.getFieldValueString(LineCnab240v05HeaderLoteExtrato.LAYOUT);
+        String numeroVersaoLayout = line.getFieldValueString(LineCnab240v05HeaderLoteExtratoLayout031.LAYOUT);
         //08
-        String brancos = line.getFieldValueString(LineCnab240v05HeaderLoteExtrato.BRANCOS);
+        String brancos = line.getFieldValueString(LineCnab240v05HeaderLoteExtratoLayout031.BRANCOS);
         //09
-        int tipoInscricaoEmpresa = line.getFieldValueNumber(LineCnab240v05HeaderLoteExtrato.TIPO_INSCRICAO_EMPRESA).intValue();
+        int tipoInscricaoEmpresa = line.getFieldValueNumber(LineCnab240v05HeaderLoteExtratoLayout031.TIPO_INSCRICAO_EMPRESA).intValue();
         //10
-        long inscricaoEmpresa = line.getFieldValueNumber(LineCnab240v05HeaderLoteExtrato.NUM_INSCRICAO_EMPRESA).longValue();
+        long inscricaoEmpresa = line.getFieldValueNumber(LineCnab240v05HeaderLoteExtratoLayout031.NUM_INSCRICAO_EMPRESA).longValue();
         //11
-        String tipoConta = line.getFieldValueString(LineCnab240v05HeaderLoteExtrato.TIPO_CONTA);
+        String tipoConta = line.getFieldValueString(LineCnab240v05HeaderLoteExtratoLayout031.TIPO_CONTA);
         //12
-        String brancos2 = line.getFieldValueString(LineCnab240v05HeaderLoteExtrato.BRANCOS2);
+        String brancos2 = line.getFieldValueString(LineCnab240v05HeaderLoteExtratoLayout031.BRANCOS2);
         //13
-        String convenio = line.getFieldValueString(LineCnab240v05HeaderLoteExtrato.CONVENIO);
+        String convenio = line.getFieldValueString(LineCnab240v05HeaderLoteExtratoLayout031.CONVENIO);
         //14
-        String zeros = line.getFieldValueString(LineCnab240v05HeaderLoteExtrato.ZEROS);
+        String zeros = line.getFieldValueString(LineCnab240v05HeaderLoteExtratoLayout031.ZEROS);
         //15
-        int agenciaCedente = line.getFieldValueNumber(LineCnab240v05HeaderLoteExtrato.AGENCIA_MANTENEDORA).intValue();
+        int agenciaCedente = line.getFieldValueNumber(LineCnab240v05HeaderLoteExtratoLayout031.AGENCIA_MANTENEDORA).intValue();
         //16
-        String digitoAgenciaCedente = line.getFieldValueString(LineCnab240v05HeaderLoteExtrato.DIGITO_VER_AGENCIA);
+        String digitoAgenciaCedente = line.getFieldValueString(LineCnab240v05HeaderLoteExtratoLayout031.DIGITO_VER_AGENCIA);
         //17
-        String zeros2 = line.getFieldValueString(LineCnab240v05HeaderLoteExtrato.ZEROS2);
+        String zeros2 = line.getFieldValueString(LineCnab240v05HeaderLoteExtratoLayout031.ZEROS2);
         //18
-        int numeroContaCorrente = line.getFieldValueNumber(LineCnab240v05HeaderLoteExtrato.NUMERO_CONTA_COR).intValue();
+        int numeroContaCorrente = line.getFieldValueNumber(LineCnab240v05HeaderLoteExtratoLayout031.NUMERO_CONTA_COR).intValue();
         //19
-        String brancos3 = line.getFieldValueString(LineCnab240v05HeaderLoteExtrato.BRANCOS3);
+        String brancos3 = line.getFieldValueString(LineCnab240v05HeaderLoteExtratoLayout031.BRANCOS3);
         //20
-        int digitoVerificadorAgConta = line.getFieldValueNumber(LineCnab240v05HeaderLoteExtrato.DIGITO_VER_AG_CONTA).intValue();
+        int digitoVerificadorAgConta = line.getFieldValueNumber(LineCnab240v05HeaderLoteExtratoLayout031.DIGITO_VER_AG_CONTA).intValue();
         //21
-        String nomeEmpresa = line.getFieldValueString(LineCnab240v05HeaderLoteExtrato.NOME_EMPRESA);
+        String nomeEmpresa = line.getFieldValueString(LineCnab240v05HeaderLoteExtratoLayout031.NOME_EMPRESA);
         //22
-        String brancos4 = line.getFieldValueString(LineCnab240v05HeaderLoteExtrato.BRANCOS4);
+        String brancos4 = line.getFieldValueString(LineCnab240v05HeaderLoteExtratoLayout031.BRANCOS4);
         //23
-        Date dataInicial = line.getFieldValueDate(LineCnab240v05HeaderLoteExtrato.DATA_SALDO_INICIAL);
+        Date dataInicial = line.getFieldValueDate(LineCnab240v05HeaderLoteExtratoLayout031.DATA_SALDO_INICIAL);
         //24
-        double valorSaldoInicial = line.getFieldValueNumber(LineCnab240v05HeaderLoteExtrato.VALOR_SALDO_INICIAL).doubleValue();
+        double valorSaldoInicial = line.getFieldValueNumber(LineCnab240v05HeaderLoteExtratoLayout031.VALOR_SALDO_INICIAL).doubleValue();
         //25
-        String situacaoInicial = line.getFieldValueString(LineCnab240v05HeaderLoteExtrato.SITUACAO_INICIAL);
+        String situacaoInicial = line.getFieldValueString(LineCnab240v05HeaderLoteExtratoLayout031.SITUACAO_INICIAL);
         //26
-        String statusInicial = line.getFieldValueString(LineCnab240v05HeaderLoteExtrato.STATUS_INICIAL);
+        String statusInicial = line.getFieldValueString(LineCnab240v05HeaderLoteExtratoLayout031.STATUS_INICIAL);
         //27
-        String tipoMoeda = line.getFieldValueString(LineCnab240v05HeaderLoteExtrato.TIPO_MOEDA);
+        String tipoMoeda = line.getFieldValueString(LineCnab240v05HeaderLoteExtratoLayout031.TIPO_MOEDA);
         //28
-        int sequenciaExtrato = line.getFieldValueNumber(LineCnab240v05HeaderLoteExtrato.SEQUENCIA_EXTRATO).intValue();
+        int sequenciaExtrato = line.getFieldValueNumber(LineCnab240v05HeaderLoteExtratoLayout031.SEQUENCIA_EXTRATO).intValue();
         //29
-        String brancos5 = line.getFieldValueString(LineCnab240v05HeaderLoteExtrato.BRANCOS5);
+        String brancos5 = line.getFieldValueString(LineCnab240v05HeaderLoteExtratoLayout031.BRANCOS5);
         
         System.out.println("01. Código banco: " + codigoBanco);
         System.out.println("02. Lote: " + loteServico);
