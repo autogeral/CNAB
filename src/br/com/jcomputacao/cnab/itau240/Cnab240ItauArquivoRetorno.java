561664185301a7cda6203df0ae132c13f9e87eaf
Larissa-Antunes <larissa.antunes12357@gmail.com>
2021-09-16 17:48:26 -0300

    [add]criando uma classe para processar o arquivo retorno

--- src/br/com/jcomputacao/cnab/itau240/Cnab240ItauArquivoRetorno.java
+++ src/br/com/jcomputacao/cnab/itau240/Cnab240ItauArquivoRetorno.java
@@ -1,35 +1,94 @@
 package br.com.jcomputacao.cnab.itau240;
 
+import com.ancientprogramming.fixedformat4j.format.FixedFormatManager;
+import com.ancientprogramming.fixedformat4j.format.impl.FixedFormatManagerImpl;
+import java.util.List;
+
 /**
  *
  * @author larissa.camargo
  *   15.09.2021
  */
 public class Cnab240ItauArquivoRetorno {
+    
     public void tratarLinha(Cnab240ItauLoteRetorno loteRetorno, String linha) {
         if (linha == null) {
             return;
         }
-        if (linha.startsWith("0")) {
-            trataHeaderRetorno(linha, loteRetorno);
+        String tipoRegistro = linha.substring(7, 8);
+        if (tipoRegistro.startsWith("0")) {
+            trataHeaderRemessa(linha, loteRetorno);
         }
-        if (linha.startsWith("1")){
-            trataRegistro(linha, loteRetorno);
+        if(tipoRegistro.startsWith("1")){
+            trataHeaderLote(linha, loteRetorno);
         }
-        if (linha.startsWith("9")){
-            trataRegistroTrailer(linha, loteRetorno);            
-        }     
+        if (tipoRegistro.startsWith("3")){
+            trataSegmento(linha, loteRetorno);
+        }
+        if (tipoRegistro.startsWith("5")){
+            trataTrailerLote(linha, loteRetorno);
+        }
+        if (tipoRegistro.startsWith("9")){
+            trataTrailerArquivo(linha, loteRetorno);
+        }
+        
+        String tipoOperacao = linha.substring(9, 9);
+        if (tipoOperacao.startsWith("T")) {
+            trataHeaderLote(linha, loteRetorno);
+        }
+        
+        String tipoSegmento = linha.substring(14, 15);
+        if (tipoSegmento.startsWith("T")) {
+            trataSegmentoT(linha, loteRetorno);
+        }
+        if (tipoSegmento.startsWith("U")){
+            trataSegmentoU(linha, loteRetorno);
+        }
     }
-    
-    private void trataHeaderRetorno(String linha, Cnab240ItauLoteRetorno loteRetorno) {
-         
+    private void trataHeaderRemessa(String linha, Cnab240ItauLoteRetorno loteRetorno) {
+        FixedFormatManager manager = new FixedFormatManagerImpl();
+        LineCnabRegistroHeaderRemessa lr = new LineCnabRegistroHeaderRemessa();
+        lr = manager.load(LineCnabRegistroHeaderRemessa.class, linha);        
+        loteRetorno.setHeaderArquivo(lr);
     }
-    
-    private void trataRegistro(String linha, Cnab240ItauLoteRetorno loteRetorno) {
+     private void trataHeaderLote(String linha, Cnab240ItauLoteRetorno loteRetorno) {
+        FixedFormatManager manager = new FixedFormatManagerImpl();
+        LineCnabRegistroHeaderLoteRemessa lr = new LineCnabRegistroHeaderLoteRemessa();
+        lr = manager.load(LineCnabRegistroHeaderLoteRemessa.class, linha);        
+        loteRetorno.setHeaderLote(lr);  
+     }
+    private void trataSegmento(String linha, Cnab240ItauLoteRetorno loteRetorno) {
+        String segmento = linha.substring(13, 14);
+        if("T".equals(segmento)) {
+            trataSegmentoT(linha, loteRetorno);
+        } else if("U".equals(segmento)) {
+            trataSegmentoU(linha, loteRetorno);
+        }
         
     }
-    
-    private void trataRegistroTrailer(String linha, Cnab240ItauLoteRetorno loteRetorno) {
-         
+    private void trataSegmentoT(String linha, Cnab240ItauLoteRetorno loteRetorno) {
+        FixedFormatManager manager = new FixedFormatManagerImpl();
+        LineCnabRegistroSegmentoTRetorno lr = new LineCnabRegistroSegmentoTRetorno();
+        lr = manager.load(LineCnabRegistroSegmentoTRetorno.class, linha);
+        List<LineCnabRegistroSegmentoTRetorno> tipo1Lista = loteRetorno.getSegmentoTRetorno();     
+        tipo1Lista.add(lr);        
+    }
+    private void trataSegmentoU(String linha, Cnab240ItauLoteRetorno loteRetorno) {
+        FixedFormatManager manager = new FixedFormatManagerImpl();
+        LineCnabRegistroSegmentoURetorno lr = new LineCnabRegistroSegmentoURetorno();
+        lr = manager.load(LineCnabRegistroSegmentoURetorno.class, linha);        
+        loteRetorno.getSegmentoURetorno().add(lr);
+    }
+    private void trataTrailerLote(String linha, Cnab240ItauLoteRetorno loteRetorno) {
+        FixedFormatManager manager = new FixedFormatManagerImpl();
+        LineCnabRegistroTrailerLote lr = new LineCnabRegistroTrailerLote();
+        lr = manager.load(LineCnabRegistroTrailerLote.class, linha);      
+        loteRetorno.setTrailerLote(lr); 
+    }
+    private void trataTrailerArquivo(String linha, Cnab240ItauLoteRetorno loteRetorno) {
+        FixedFormatManager manager = new FixedFormatManagerImpl();
+        LineCnabRegistroTrailerArquivo lr = new LineCnabRegistroTrailerArquivo();
+        lr = manager.load(LineCnabRegistroTrailerArquivo.class, linha); 
+        loteRetorno.setTrailerArquivo(lr);
     }
 }
--- src/br/com/jcomputacao/cnab/itau240/Cnab240ItauLoteRetorno.java
+++ src/br/com/jcomputacao/cnab/itau240/Cnab240ItauLoteRetorno.java
@@ -1,70 +1,66 @@
 package br.com.jcomputacao.cnab.itau240;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author larissa.camargo
  *    15.09.2021
  */
 public class Cnab240ItauLoteRetorno {
-    // Line Cnab Registro Segmento T 
-    private String codigoBancoT;
-    private String codigoLoteT;
-    private String tipoRegistroT;
-    private String numRegistroT;
-    private String segmentoT;
-    private String boletoDdaT;
-    private String codOcorrenciaT;
-    private String zeros1T;
-    private String agenciaT;
-    private String zeros2T;
-    private String contaT;
-    private String zeros3T;
-    private String dac1T;
-    private String numCarteiraT;
-    private String nossoNumeroT;
-    private String dac2T;
-    private String brancos1T;
-    private String zeros4T;
-    private String seuNumeroT;
-    private String brancos2T;
-    private String vencimentoT;
-    private String valorTituloT;
-    private String zeros5T;
-    private String agCobradoraT;
-    private String dac3T;
-    private String usoEmpresaT;
-    private String zeros6T;
-    private String codigoInscricaoT;
-    private String inscricaoNumeroT;
-    private String nomeT;
-    private String brancos3T;
-    private String zeros7T;
-    private String tarifaCustasT;
-    private String errosT;
-    private String codLiquidacaoT;
-    private String brancos4T;
-    
-    // Line Cnab Registro Segmento U
-    private String codigoBancoU;
-    private String codigoLoteU;
-    private String registroU;
-    private String numRegistroU;
-    private String segmentoU;
-    private String brancos1U;
-    private String codOcorrrenciaU;
-    private String jurosMultaU;
-    private String valorDescontoU;
-    private String valorAbatimentoU;
-    private String valorIofU;
-    private String valorCreditado1U;
-    private String valorCreditado2U;
-    private String zeros1U;
-    private String dataOcorrencia1U;
-    private String dataCreditoU;
-    private String ocorrenciaPagadorU;
-    private String dataOcorrencia2U;
-    private String valorOcorrenciaU;
-    private String brancos2U;
-    private String zeros2U;
-    private String brancos3U;
+    private LineCnabRegistroHeaderRemessa headerArquivo;
+    private LineCnabRegistroHeaderLoteRemessa headerLote;
+    private List<LineCnabRegistroSegmentoTRetorno> segmentoTRetorno = new ArrayList<LineCnabRegistroSegmentoTRetorno>();    
+    private List<LineCnabRegistroSegmentoURetorno> segmentoURetorno = new ArrayList<LineCnabRegistroSegmentoURetorno>();
+    private LineCnabRegistroTrailerLote trailerLote;
+    private LineCnabRegistroTrailerArquivo trailerArquivo;
+
+    public LineCnabRegistroHeaderRemessa getHeaderArquivo() {
+        return headerArquivo;
+    }
+
+    public void setHeaderArquivo(LineCnabRegistroHeaderRemessa headerArquivo) {
+        this.headerArquivo = headerArquivo;
+    }
+
+    public LineCnabRegistroHeaderLoteRemessa getHeaderLote() {
+        return headerLote;
+    }
+
+    public void setHeaderLote(LineCnabRegistroHeaderLoteRemessa headerLote) {
+        this.headerLote = headerLote;
+    }
+
+    public List<LineCnabRegistroSegmentoTRetorno> getSegmentoTRetorno() {
+        return segmentoTRetorno;
+    }
+
+    public void setSegmentoTRetorno(List<LineCnabRegistroSegmentoTRetorno> segmentoTRetorno) {
+        this.segmentoTRetorno = segmentoTRetorno;
+    }
+
+    public List<LineCnabRegistroSegmentoURetorno> getSegmentoURetorno() {
+        return segmentoURetorno;
+    }
+
+    public void setSegmentoURetorno(List<LineCnabRegistroSegmentoURetorno> segmentoURetorno) {
+        this.segmentoURetorno = segmentoURetorno;
+    }
+
+    public LineCnabRegistroTrailerLote getTrailerLote() {
+        return trailerLote;
+    }
+
+    public void setTrailerLote(LineCnabRegistroTrailerLote trailerLote) {
+        this.trailerLote = trailerLote;
+    }
+
+    public LineCnabRegistroTrailerArquivo getTrailerArquivo() {
+        return trailerArquivo;
+    }
+
+    public void setTrailerArquivo(LineCnabRegistroTrailerArquivo trailerArquivo) {
+        this.trailerArquivo = trailerArquivo;
+    }
 }
